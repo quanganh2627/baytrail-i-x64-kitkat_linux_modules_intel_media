1894a5cd6f59c98f02de59ea9da3aa2bafce7c6f
buildslave <buildslave@buildbot.tl.intel.com>
2013-07-12 13:46:24 +0000

    Automatically revert commits from previous weekly release
    Change-Id: Ib8fe971160f9b7cb1e60be21222030a51f35f811
    Signed-off-by: buildslave <buildslave@buildbot.tl.intel.com>

--- graphics/rgx/services/server/common/srvcore.c
+++ graphics/rgx/services/server/common/srvcore.c
@@ -233,7 +233,6 @@ PVRSRVHWOpTimeoutKM(IMG_VOID)
 #endif
 	PVR_LOG(("HW operation timeout, dump server info"));
 	PVRSRVDumpDebugInfoKM(DEBUG_REQUEST_VERBOSITY_MAX);
-	panic("HW operation timeout\n");
 	return PVRSRV_OK;
 }
 
