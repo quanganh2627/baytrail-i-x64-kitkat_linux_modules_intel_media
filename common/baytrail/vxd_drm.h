b68e8d56d6534a9886c93e8c5bfb6406f13f4f15
Fei Jiang <fei.jiang@intel.com>
2014-01-25 22:33:50 +0800

    intel-media: need make vxd392 kernel driver compatbile on Baytrail platform
    BZ: 167469
    
    There is different alignment requirement for structure on 32 and 64 bit kernel,
    for example, uint64_t member need be 64 bit aligned on 64 bit kernel,
    while only require 32 bit aligned on 32 bit kernel.
    
    Change-Id: I5c8fb121a0d10e3377493ecf0d05cca962dd7bc9
    Signed-off-by: Fei Jiang <fei.jiang@intel.com>

--- common/baytrail/vxd_drm.h
+++ common/baytrail/vxd_drm.h
@@ -142,8 +142,8 @@ struct psb_validate_rep {
 #define PSB_USE_PRESUMED     (1 << 0)
 
 struct psb_validate_arg {
-	int handled;
-	int ret;
+	uint64_t handled;
+	uint64_t ret;
 	union {
 		struct psb_validate_req req;
 		struct psb_validate_rep rep;
@@ -203,7 +203,7 @@ enum lnc_getparam_key {
 };
 
 struct drm_lnc_video_getparam_arg {
-	enum lnc_getparam_key key;
+	uint64_t key;
 	uint64_t arg;		/* argument pointer */
 	uint64_t value;		/* feed back pointer */
 };
