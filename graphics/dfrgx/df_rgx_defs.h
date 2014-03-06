53b5bf69d3b3675e590919cbaf5ffb0363b3ab87
Khalil, Sarrah <sarrah.khalil@intel.com>
2014-03-06 03:25:30 -0800

    Revert "[Graphics]Revert "[DEVFREQ]:DevFreq thermal condition corner case.""
    BZ: 177285
    
    This reverts commit c4a3e8fe1030ada9b760456bb0b3775f40c1dc40.
    this patch reverts https://android.intel.com/#/c/167385/
    
    Change-Id: I11ecc998145f41cc26a476c924532ac22f80bde3
    Signed-off-by: Sarrah Khalil <Sarrah.khalil@intel.com>

--- graphics/dfrgx/df_rgx_defs.h
+++ graphics/dfrgx/df_rgx_defs.h
@@ -159,6 +159,7 @@ struct busfreq_data {
 	unsigned long int     bf_prev_freq_rlzd;
 	unsigned long int     bf_desired_freq;
 	unsigned int	      b_resumed;
+	unsigned int	      b_need_freq_update;
 	char		      prev_governor[DEVFREQ_NAME_LEN + 1];
 
 	struct thermal_cooling_device *gbp_cooldv_hdl;
