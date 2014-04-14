12ef110f9d2884348fd303f6da1a299d51558572
Ren, Zhaohan <zhaohan.ren@intel.com>
2014-04-14 10:07:57 +0800

    Refine spid-firmware table and firmware uploading process
    BZ:187532
    
    This patch refined and complemented spid-fw table, driver will
    load the matched firmware or the nearest firmware based on spid.
    
    Change-Id: I4e4e89b3cb5d6f481a0fbbf04475d11e8e8085e9
    Signed-off-by: Ren, Zhaohan <zhaohan.ren@intel.com>

--- video/common/tng_securefw.c
+++ video/common/tng_securefw.c
@@ -52,12 +52,52 @@ extern struct soft_platform_id spid;
 /*
  * Common firmware files shared by differnt product/hardware
  */
-#define mrfl_pr2_msvdx  "signed_msvdx_fw_mrfld_b0v1.bin"
-#define mrfl_pr2_topaz  "topazhp_fw_b0.bin"
-#define mrfl_pr2_vsp    "vsp_vpp_vp8_b0.bin"
-#define mofd_pr2_msvdx  "ann_a0_signed_ved_key0.bin"
-#define mofd_pr2_topaz  "ann_a0_signed_vec_key0.bin"
-#define mofd_pr2_vsp    "ann_signed_vsp_a0key0.bin"
+#define mrfl_ffrd_pr2_b0_msvdx	"msvdx.bin.0004.0000.000d"
+#define mrfl_vv_fab_b_b0_msvdx	"msvdx.bin.0004.0000.0001"
+#define mrfl_vv_msvdx		mrfl_vv_fab_b_b0_msvdx
+#define mrfl_product_msvdx	"msvdx.bin.0004.0000.0015"
+#define mrfl_pr2_macro_b0_msvdx	"msvdx.bin.0004.0000.000c"
+#define mrfl_sand_msvdx		"msvdx.bin.0004.0002.0001"
+#define mofd_vv_fab_a_msvdx	"msvdx.bin.0008.0000.0000"
+#define mofd_v0_msvdx		"msvdx.bin.0008.0000.0001"
+#define mofd_v1_pr2_msvdx	"msvdx.bin.0008.0002.0001"
+#define mofd_ffrd_pr0_msvdx	mofd_v0_msvdx
+//#define mofd_product_msvdx	"mofd_product_msvdx.bin"
+#define mofd_prh_b0_msvdx	"msvdx.bin.000c.0001.0001"
+
+#define mrfl_ffrd_pr2_b0_topaz	"topaz.bin.0004.0000.000d"
+#define mrfl_vv_fab_b_b0_topaz	"topaz.bin.0004.0000.0001"
+#define mrfl_vv_topaz		mrfl_vv_fab_b_b0_topaz
+#define mrfl_pr2_macro_b0_topaz	"topaz.bin.0004.0000.000c"
+#define mrfl_product_topaz	"topaz.bin.0004.0000.0015"
+#define mrfl_sand_topaz		"topaz.bin.0004.0002.0001"
+#define mofd_vv_fab_a_topaz	"topaz.bin.0008.0000.0000"
+#define mofd_v0_topaz		"topaz.bin.0008.0000.0001"
+#define mofd_v1_pr2_topaz	"topaz.bin.0008.0002.0001"
+#define mofd_ffrd_pr0_topaz	mofd_vv_fab_a_topaz
+//#define mofd_product_topaz	"mofd_product_topaz.bin"
+#define mofd_prh_b0_topaz	"topaz.bin.000c.0001.0001"
+
+#define mrfl_ffrd_pr2_b0_vsp	"vsp.bin.0004.0000.000d"
+#define mrfl_vv_fab_b_b0_vsp	"vsp.bin.0004.0000.0001"
+#define mrfl_vv_vsp		mrfl_vv_fab_b_b0_vsp
+#define mrfl_product_vsp	"vsp.bin.0004.0000.0015"
+#define mrfl_sand_vsp		"vsp.bin.0004.0002.0001"
+#define mrfl_pr2_macro_b0_vsp	"vsp.bin.0004.0000.000c"
+#define mofd_vv_fab_a_vsp	"vsp.bin.0008.0000.0000"
+#define mofd_v0_vsp		"vsp.bin.0008.0000.0001"
+#define mofd_v1_pr2_vsp		"vsp.bin.0008.0002.0001"
+#define mofd_ffrd_pr0_vsp	mofd_vv_fab_a_vsp
+//#define mofd_product_vsp	"mofd_product_vsp.bin"
+#define mofd_prh_b0_vsp		"vsp.bin.000c.0001.0001"
+
+#define mrfl_defalut_msvdx	mrfl_product_msvdx
+#define mrfl_defalut_topaz	mrfl_product_topaz
+#define mrfl_defalut_vsp	mrfl_product_vsp
+
+#define mofd_defalut_msvdx	mofd_v0_msvdx
+#define mofd_defalut_topaz	mofd_v0_topaz
+#define mofd_defalut_vsp	mofd_v0_vsp
 
 /*
  * Firmware name if there is no entry in spid2fw table
@@ -79,11 +119,34 @@ struct spid2fw_mapping {
 /*
  * Table spid2fw
  */
-static struct spid2fw_mapping  spid2fw[] = {
-	{4, 0, 0xd, mrfl_pr2_msvdx, mrfl_pr2_topaz, mrfl_pr2_vsp, 15}, /* merrifield pr2 */
-	{8, 0, 0xd, mofd_pr2_msvdx, mofd_pr2_topaz, mofd_pr2_vsp, 15}, /* moorefield pr2 */ 
+static struct spid2fw_mapping spid2fw[] = {
+	{4, 0, 0, mrfl_vv_msvdx, mrfl_vv_topaz, mrfl_vv_vsp, 15}, /* merrifield VV */
+	{4, 0, 1, mrfl_vv_fab_b_b0_msvdx, mrfl_vv_fab_b_b0_topaz, mrfl_vv_fab_b_b0_vsp, 15}, /* merrifield VV Fab B (B0) */
+	//{4, 0, 0xc, mrfl_pr2_macro_b0_msvdx, mrfl_pr2_macro_b0_topaz, mrfl_pr2_macro_b0_vsp, 15}, /* merrifield PR2 Macro (B0) */
+	{4, 0, 0xd, mrfl_ffrd_pr2_b0_msvdx, mrfl_ffrd_pr2_b0_topaz, mrfl_ffrd_pr2_b0_vsp, 15}, /* merrifield FFRD PR2 (B0) */
+	{4, 0, 15, mrfl_product_msvdx, mrfl_product_topaz, mrfl_product_vsp, 15}, /* merrifield product */
+	{4, 2, 1, mrfl_sand_msvdx, mrfl_sand_topaz, mrfl_sand_vsp, 0}, /* merrifield SAND */
+
+	{8, 0, 0, mofd_vv_fab_a_msvdx, mofd_vv_fab_a_topaz, mofd_vv_fab_a_vsp, 15}, /* moorefield VV Fab A */
+	{8, 0, 1, mofd_v0_msvdx, mofd_v0_topaz, mofd_v0_vsp, 15}, /* moorefield V0 */
+	{8, 2, 0, mofd_vv_fab_a_msvdx, mofd_vv_fab_a_topaz, mofd_vv_fab_a_vsp, 15}, /* moorefield V1 VV with A0 soc */
+	{8, 2, 1, mofd_v1_pr2_msvdx, mofd_v1_pr2_topaz, mofd_v1_pr2_vsp, 15}, /* moorefield V1 PR2 */
+	//{8, 0, 2, mofd_product_msvdx, mofd_product_topaz, mofd_product_vsp, 15}, /* Anniedale Production Keys (QS/PRQ) */
+	{0xc, 0, 4, mofd_ffrd_pr0_msvdx, mofd_ffrd_pr0_topaz, mofd_ffrd_pr0_vsp, 15}, /* moorefield FFRD PR0 */
+	{0xc, 1, 1, mofd_prh_b0_msvdx, mofd_prh_b0_topaz, mofd_prh_b0_vsp, 15}, /* MCG Moorefield PRH B0 */
+
 	{-1, -1, -1, NULL, NULL, NULL, 15} /* the last entry for non-supported device */
 };
+
+/*
+* Fall back once mismatch
+*/
+static struct spid2fw_mapping default_mrfl_spid2fw =
+	{4, 0, 15, mrfl_product_msvdx, mrfl_product_topaz, mrfl_product_vsp, 15}; /* merrifield product */
+
+static struct spid2fw_mapping default_mofd_spid2fw =
+	{8, 0, 1, mofd_v0_msvdx, mofd_v0_topaz, mofd_v0_vsp, 15}; /* moorefield V0 */
+
 #define SPID2FW_NUMBER sizeof(spid2fw)/sizeof(struct spid2fw_mapping)
 static struct spid2fw_mapping *matched_spid2fw = NULL; /* query once, use multiple times */
 
@@ -93,12 +156,6 @@ static struct spid2fw_mapping *matched_spid2fw = NULL; /* query once, use multip
 #define ID_MATCH(_spid, _spid2fw) ((_spid.platform_family_id == _spid2fw->family_id) && \
 				   (_spid.product_line_id == _spid2fw->product_id) && \
 				   (_spid.hardware_id == _spid2fw->hardware_id))
-/*
- * Get the closest device entry according to spid
- */
-#define ID_MATCH_WEIGHT(_spid, _spid2fw) (4 * (_spid.platform_family_id == _spid2fw->family_id) + \
-					  2 * (_spid.product_line_id == _spid2fw->product_id) + \
-					  (_spid.hardware_id == _spid2fw->hardware_id))
 
 static void tng_copyfw(char *fw_name, char *island_name, int *sep_key, struct spid2fw_mapping *p)
 {
@@ -117,11 +174,11 @@ static void tng_copyfw(char *fw_name, char *island_name, int *sep_key, struct sp
 	return;
 }
 
-static void tng_spid2fw(struct drm_device *dev, char *fw_name, char *island_name, int *sep_key)
+static void tng_spid2fw(struct drm_device *dev, char *fw_name, char *fw_basename, char *island_name, int *sep_key)
 {
-	struct spid2fw_mapping *nearest_spid2fw, *p;
+	struct spid2fw_mapping *p;
 	const struct firmware *raw = NULL;
-	int i, ret, tmp, max_match = -1;
+	int i, ret;
 
 	/* aready get the matched entry in spid2fw table */
 	if (matched_spid2fw) {
@@ -130,17 +187,11 @@ static void tng_spid2fw(struct drm_device *dev, char *fw_name, char *island_name
 	}
 
 	/* try to find the matched entry in spid2fw table */
-	p = nearest_spid2fw = spid2fw;
-	for (i=0; i<SPID2FW_NUMBER; i++) {
+	p = spid2fw;
+	for (i = 0; i < SPID2FW_NUMBER; i++) {
 		if (ID_MATCH(spid, p)) {
 			matched_spid2fw = p;
 			break;
-		}
-
-		tmp = ID_MATCH_WEIGHT(spid, p);
-		if (max_match < tmp) {
-			max_match = tmp;
-			nearest_spid2fw = p;
 		}
 
 		p++;
@@ -149,6 +200,7 @@ static void tng_spid2fw(struct drm_device *dev, char *fw_name, char *island_name
 	/* find a matched entry */
 	if (matched_spid2fw != NULL) {
 		tng_copyfw(fw_name,  island_name, sep_key, matched_spid2fw);
+		PSB_DEBUG_INIT("Got matched firmware %s\n", fw_name, fw_name);
 		return;
 	}
 
@@ -156,16 +208,23 @@ static void tng_spid2fw(struct drm_device *dev, char *fw_name, char *island_name
 	 * No entry in the table, fake one
 	 * Firstly try if we have the firmware like mvsdx.bin.0004.0002.000d
 	 */
-	DRM_ERROR("Cannot find pre-defined firmware for this spid, try to detect the firmware\n");
-	snprintf(fw_name, FW_NAME_LEN, "msvdx.bin.%04x.%04x.%04x",
-		 (int)spid.platform_family_id,
-		 (int)spid.product_line_id,
-		 (int)spid.hardware_id);
+	DRM_ERROR("Cannot find pre-defined firmware for this spid, try to detect %s firmware\n", fw_basename);
+
+	snprintf(fw_name, FW_NAME_LEN, "%s.bin.%04x.%04x.%04x",
+		fw_basename,
+		(int)spid.platform_family_id,
+		(int)spid.product_line_id,
+		(int)spid.hardware_id);
+
 	ret = request_firmware(&raw, fw_name, &dev->pdev->dev);
 	if (raw == NULL || ret < 0) { /* there is no mvsdx.bin.0004.0002.000d */
-		DRM_ERROR("There is no firmware %s, try to use the closest device firmware\n", fw_name);
+		DRM_ERROR("There is no firmware %s, try to use the default device firmware\n", fw_name);
 
-		matched_spid2fw = nearest_spid2fw;
+		if ((int)spid.platform_family_id == 4)
+			matched_spid2fw = &default_mrfl_spid2fw;
+		else
+			matched_spid2fw = &default_mofd_spid2fw;
+
 		tng_copyfw(fw_name,  island_name, sep_key, matched_spid2fw);
 		return;
 	}
@@ -175,33 +234,37 @@ static void tng_spid2fw(struct drm_device *dev, char *fw_name, char *island_name
 	 */
 	release_firmware(raw);
 
-	matched_spid2fw = &spid2fw[SPID2FW_NUMBER-1];
+	PSB_DEBUG_INIT("Detect %s firmware %s success! Fake entries for all the other islands\n", fw_basename, fw_name);
+
+	matched_spid2fw = &spid2fw[SPID2FW_NUMBER - 1];
 
 	matched_spid2fw->msvdx_fw = default_msvdx;
 	snprintf(matched_spid2fw->msvdx_fw, FW_NAME_LEN, "msvdx.bin.%04x.%04x.%04x",
 		 (int)spid.platform_family_id, (int)spid.product_line_id, (int)spid.hardware_id);
 	
 	matched_spid2fw->topaz_fw = default_topaz;
-	snprintf(matched_spid2fw->msvdx_fw, FW_NAME_LEN, "topaz.bin.%04x.%04x.%04x",
+	snprintf(matched_spid2fw->topaz_fw, FW_NAME_LEN, "topaz.bin.%04x.%04x.%04x",
 		 (int)spid.platform_family_id, (int)spid.product_line_id, (int)spid.hardware_id);
 	
 	matched_spid2fw->vsp_fw = default_vsp;
-	snprintf(matched_spid2fw->msvdx_fw, FW_NAME_LEN, "vsp.bin.%04x.%04x.%04x",
+	snprintf(matched_spid2fw->vsp_fw, FW_NAME_LEN, "vsp.bin.%04x.%04x.%04x",
 		 (int)spid.platform_family_id, (int)spid.product_line_id, (int)spid.hardware_id);
 
 	/* force the sep key with the nearest one*/
-	matched_spid2fw->sep_key = nearest_spid2fw->sep_key;
+	if ((int)spid.platform_family_id == 4)
+		matched_spid2fw->sep_key =  default_mrfl_spid2fw.sep_key;
+	else
+		matched_spid2fw->sep_key =  default_mofd_spid2fw.sep_key;
 
 	tng_copyfw(fw_name, island_name, sep_key, matched_spid2fw);
 	return;
 }
 
-
 static void tng_get_fwinfo(struct drm_device *dev, char *fw_name, char *fw_basename, char *island_name, int *sep_key)
 {
-	PSB_DEBUG_INIT("SPID: family_id.product_id.hardware_id=0x%04x.0x%04x.0x%04x\n",
+	DRM_INFO("SPID: family_id.product_id.hardware_id=0x%04x.0x%04x.0x%04x\n",
 		       spid.platform_family_id, spid.product_line_id, spid.hardware_id);
-#if 1
+#if 0
 	snprintf(fw_name, FW_NAME_LEN, "%s.bin.%04x.%04x",
 		 fw_basename, (int)spid.platform_family_id, (int)spid.hardware_id);
 
@@ -214,7 +277,7 @@ static void tng_get_fwinfo(struct drm_device *dev, char *fw_name, char *fw_basen
 	
 	return;
 #else
-	tng_spid2fw(dev, fw_name, island_name, sep_key);
+	tng_spid2fw(dev, fw_name, fw_basename, island_name, sep_key);
 #endif
 	
 	PSB_DEBUG_INIT("Use firmware %s for %s, SEP key %d\n", fw_name, island_name, *sep_key);
@@ -350,7 +413,7 @@ int tng_securefw(struct drm_device *dev, char *fw_basename, char *island_name, i
 	PSB_DEBUG_INIT("Try to request firmware %s\n", fw_name);
 	ret = request_firmware(&raw, fw_name, &dev->pdev->dev);
 	if (raw == NULL || ret < 0) {
-		DRM_ERROR("Failed to request firmware, ret =  %d\n", ret);
+		DRM_ERROR("Failed to request firmware %s, ret =  %d\n", fw_name, ret);
 		return ret;
 	}
 
