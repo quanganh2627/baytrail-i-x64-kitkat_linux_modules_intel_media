99e9ff2ec8d72da8a1bc5d542133bcad4ea4fb0d
Marc Mao <marc.mao@intel.com>
2014-05-07 11:13:59 +0800

    Implement DisplayContext flush kernel mode
    BZ: 197822
    
    This is a workaround patch from Ticket 29847 to fix dead lock caused
    by multiple context in same process.
    
    Change-Id: I4bc89a76d65eb28ecbbc1118f22d2a104f3b5f94
    Signed-off-by: Marc Mao <marc.mao@intel.com>

--- graphics/rgx/services/server/common/scp.c
+++ graphics/rgx/services/server/common/scp.c
@@ -720,6 +720,7 @@ IMG_EXPORT
 PVRSRV_ERROR SCPRun(SCP_CONTEXT *psContext)
 {
 	SCP_COMMAND *psCommand;
+	PVRSRV_ERROR eError = PVRSRV_OK;
 
 	if (psContext == IMG_NULL)
 	{
@@ -729,8 +730,6 @@ PVRSRV_ERROR SCPRun(SCP_CONTEXT *psContext)
 	OSLockAcquire(psContext->hLock);
 	while (psContext->ui32DepOffset != psContext->ui32WriteOffset)
 	{
-		PVRSRV_ERROR eError;
-
 		psCommand = (SCP_COMMAND *)((IMG_UINT8 *)psContext->pvCCB +
 		            psContext->ui32DepOffset);
 
@@ -758,7 +757,7 @@ PVRSRV_ERROR SCPRun(SCP_CONTEXT *psContext)
 	}
 	OSLockRelease(psContext->hLock);
 
-	return PVRSRV_OK;
+	return eError;
 }
 
 IMG_EXPORT
